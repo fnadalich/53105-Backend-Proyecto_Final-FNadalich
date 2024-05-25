ebebc4d6d6242ad23b0e7e554ebc7f13669c1b89
Fernando Nadalich <f.nadalich@gmail.com>
2024-05-25 00:36:24 -0300

    Mejora en la variables de entorno

--- src/routes/views/cart.views.router.js
+++ src/routes/views/cart.views.router.js
@@ -4,6 +4,6 @@ const ViewController = require("../../controllers/view.controller.js")
 const viewController = new ViewController
 const checkUserRole = require("../../middleware/checkRole.js")
 
-router.get("/",checkUserRole(["user"]), viewController.chat)
+router.get("/:cid", checkUserRole(["user"]), viewController.cartById)
 
 module.exports = router
